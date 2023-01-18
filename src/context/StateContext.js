6341ec020722e299028f19ab27ffdc82cc979fea
Michael Buffone <m_buffone@hotmail.com>
2023-01-18 14:17:34 -0500

    added StateContext to implement adding items to cart

---
+++ src/context/StateContext.js
@@ -0,0 +1,73 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [qty, setQty] = useState(1);
+
+  //adding products to cart
+  const onAdd = (product, quantity) => {
+    //finding item by id
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+    //changing the total price
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    //changing the total qunatity
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  };
+
+  // adding items to cart with previous values
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+  // decreasing items to cart with previous values
+  const decQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+
+      return prevQty - 1;
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(Context);
